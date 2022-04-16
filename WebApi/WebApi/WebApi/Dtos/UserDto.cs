d7e1d240ac7acd97246fce0d80979a4edb8c4c0a
Eric Saboia <saboia.eric@gmail.com>
2022-04-16 02:46:31 -0300

    feat: add domain models and dtos for user.

---
+++ WebApi/WebApi/WebApi/Dtos/UserDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WebApi.Dtos
+{
+    public class UserDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public bool IsEnabled { get; set; }
+    }
+}
