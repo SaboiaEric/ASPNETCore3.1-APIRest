5691db42ccf16f810bf4643539ba1ef2b389f8ff
Eric Saboia <saboia.eric@gmail.com>
2022-04-16 13:21:45 -0300

    feat: include app configuration connections

--- WebApi/WebApi/WebApi/Startup.cs
+++ WebApi/WebApi/WebApi/Startup.cs
@@ -1,12 +1,15 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System;
 using System.IO;
 using System.Reflection;
+using System.Text;
 using System.Text.Json;
 
 namespace WebApi
@@ -35,6 +38,26 @@ namespace WebApi
             });
 
             services.AddMemoryCache();
+
+            var key = Encoding.ASCII.GetBytes(Configuration.GetSection("ASPNETCore3.1-APIRest:JWT:Secret").Value);
+
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
 
             services.AddSwaggerGen(c =>
             {
@@ -80,6 +103,8 @@ namespace WebApi
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
