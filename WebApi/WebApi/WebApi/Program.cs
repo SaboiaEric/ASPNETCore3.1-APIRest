fde640a30f02088343b1ea9b7cced60abc3da2c4
Eric Saboia <saboia.eric@gmail.com>
2022-04-16 01:26:04 -0300

    feat: implement usuario controller base

--- WebApi/WebApi/WebApi/Program.cs
+++ WebApi/WebApi/WebApi/Program.cs
@@ -1,19 +1,14 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace WebApi
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
-            CreateHostBuilder(args).Build().Run();
+            CreateHostBuilder(Array.Empty<string>()).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
